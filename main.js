4c807e6fc0c9aa8609f19c17d5f912a00dffad3a
duc anh <11220230@st.neu.edu.vn>
2024-05-24 10:29:56 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// var fulltime='le duc anh';
+// alert(fulltime);
+//comment trong js va html khac 
+var fullName='ducanh';
+console.log(fullName)
+let x = 5;
+let y = 5;
+const myObj = {firsname:"John", lastname:"Doe"};
+console.assert(x + y == 11, myObj);
+alert("xin chao cac ban");
+alert("thong bao quan trong");
